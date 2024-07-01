72d008019b07c8be9f1845833aaebaa4e62d82e7
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-07-01 15:58:28 -0300

    Creado mock: createMockRouter para appRouterInstance. Implementado en Home.tests

--- src/__tests__/pages/Home.test.tsx
+++ src/__tests__/pages/Home.test.tsx
@@ -1,6 +1,8 @@
+import { createMockRouter } from "@/__mocks__/createMockRouter";
 import Home from "@/app/page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 global.fetch = jest.fn(
   (): Promise<any> =>
@@ -14,15 +16,25 @@ global.fetch = jest.fn(
 );
 
 describe('Home Page - "/"', () => {
-  beforeEach(() => {
-    render(<Home />);
-  });
-
   it("Tiene que tener el nombre de la aplicacion", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     expect(screen.getByText(/INVENTARIO APP/i));
   });
 
   it("Tiene que tener una breve descripcion.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     expect(
       screen.getByText(
         /Nuestra aplicación web de inventario te permite gestionar y controlar tus productos de manera eficiente y organizada. Con una interfaz intuitiva y fácil de usar, puedes agregar, actualizar y eliminar artículos, realizar un seguimiento de las existencias. Optimiza tu inventario y evita desabastecimientos o excedentes, asegurando que siempre tengas lo que necesitas cuando lo necesitas./i
@@ -31,6 +43,13 @@ describe('Home Page - "/"', () => {
   });
 
   it("Tiene que tener un boton llamado “nuevo inventario”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     expect(
       screen.getByRole("button", {
         name: /nuevo inventario/i,
@@ -39,15 +58,34 @@ describe('Home Page - "/"', () => {
   });
 
   it("El boton “nuevo inventario” debe redireccionar a “/inventario/nuevo”.", async () => {
-    // FALTA CREAR MOCK DE ROUTER E IMPLEMENTARLO EN EL "beforeEach"
+    // MOCK IMPLEMENTADO. CORROBORAR SI FUNCIONA CORRECTAMENTE.
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     await userEvent.click(
+      // Click en el boton
       screen.getByRole("button", {
         name: /nuevo inventario/i,
       })
     );
+
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith("/inventario/nuevo");
   });
 
   it("Tiene que tener el boton “unirse a inventario”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     expect(
       screen.getByRole("button", {
         name: /unirse a inventario/i,
@@ -56,15 +94,34 @@ describe('Home Page - "/"', () => {
   });
 
   it("El boton “unirse a inventario” debe redireccionar a “/inventario/unirse”.", async () => {
-    // FALTA CREAR MOCK DE ROUTER E IMPLEMENTARLO EN EL "beforeEach"
+    // MOCK IMPLEMENTADO. CORROBORAR SI FUNCIONA CORRECTAMENTE.
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     await userEvent.click(
+      // Click en el boton
       screen.getByRole("button", {
         name: /unirse a inventario/i,
       })
     );
+
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith("/inventario/unirse");
   });
 
   it("Tiene que mostrar los inventarios en los que se encuentra.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Home />
+      </AppRouterContext.Provider>
+    );
+
     expect(screen.getByText(/heladera y freezer/i));
     expect(screen.getByText(/otros/i));
   });
