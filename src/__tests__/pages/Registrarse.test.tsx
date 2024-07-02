45194693f94fd758a3ee87b458173756c0642340
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-07-02 14:48:14 -0300

    Creado Registrarse.test

--- src/__tests__/pages/Registrarse.test.tsx
+++ src/__tests__/pages/Registrarse.test.tsx
@@ -1,20 +1,93 @@
 import { createMockRouter } from "@/__mocks__/createMockRouter";
+import Registrarse from "@/app/cuenta/registrarse/page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 describe('Registrarse page - "/cuenta/registrarse"', () => {
-  it("Tiene que tener el nombre de la APP.", () => {});
+  it("Tiene que tener el nombre de la APP.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener el logo de la APP.", () => {}); // Logo por definir
+    expect(screen.getByRole("heading", { name: /INVENTARIO APP/i }));
+  });
 
-  it("Tiene que tener el titulo de “registrarse”.", () => {});
+  it("Tiene que tener el logo de la APP.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener los inputs y labels “email”, “contraseña” y “repetir contraseña”.", () => {});
+    expect(screen.getByRole("img", { name: /inventario logo/i }));
+  }); // Logo por definir
 
-  it("Tiene que tener el boton de “registrarse”.", () => {});
+  it("Tiene que tener el titulo de “registrarse”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener el link de redireccion a iniciar sesion.", () => {});
+    expect(screen.getByText(/registrarse/i));
+  });
 
-  it("El link de redir. A iniciar sesion debe redireccionar a “/cuenta/iniciar_sesion”.", () => {});
+  it("Tiene que tener los inputs y labels “email”, “contraseña” y “repetir contraseña”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
+
+    expect(screen.getByText(/email/i));
+    expect(screen.getByText(/contraseña/i));
+    expect(screen.getByText(/repetir contraseña/i));
+  });
+
+  it("Tiene que tener el boton de “registrarse”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
+
+    expect(screen.getByRole("button", { name: "registrarse" }));
+  });
+
+  it("Tiene que tener el link de redireccion a iniciar sesion.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
+
+    expect(screen.getByRole("link", { name: /iniciar sesion/i }));
+  });
+
+  it("El link de redir. A iniciar sesion debe redireccionar a “/cuenta/iniciar_sesion”.", async () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <Registrarse />
+      </AppRouterContext.Provider>
+    );
+
+    await userEvent.click(
+      // Click en el link de registrarse
+      screen.getByRole("link", { name: /iniciar sesion/i })
+    );
+
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith("/cuenta/iniciar_sesion");
+  });
 });
