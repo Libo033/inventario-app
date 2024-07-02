a6ae0ec19b4255f48705be05219308e96c4a4c4f
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-07-02 14:55:41 -0300

    En proceso creacion RecuperarContraseña.test

--- src/__tests__/pages/RecuperarContraseña.test.tsx
+++ src/__tests__/pages/RecuperarContraseña.test.tsx
@@ -1,26 +1,118 @@
 import { createMockRouter } from "@/__mocks__/createMockRouter";
+import RecuperarContraseña from "@/app/cuenta/recuperar_contraseña/page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 describe('Recuperar Contraseña page - "/cuenta/recuperar_contraseña"', () => {
-  it("Tiene que tener el nombre de la APP.", () => {});
+  it("Tiene que tener el nombre de la APP.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener el logo de la APP.", () => {}); // Logo por definir
+    expect(screen.getByRole("heading", { name: /INVENTARIO APP/i }));
+  });
 
-  it("Tiene que tener el titulo de “recuperar contraseña”.", () => {});
+  it("Tiene que tener el logo de la APP.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener los inputs y labels “email”.", () => {});
+    expect(screen.getByRole("img", { name: /inventario logo/i }));
+  }); // Logo por definir
 
-  it("Tiene que tener el boton de “enviar”.", () => {});
+  it("Tiene que tener el titulo de “recuperar contraseña”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
 
-  it("Tiene que tener el link de redireccion a iniciar sesion.", () => {});
+    expect(screen.getByText(/recuperar contraseña/i));
+  });
 
-  it("El link de redir. A iniciar sesion debe redireccionar a “/cuenta/iniciar_sesion”.", () => {});
+  it("Tiene que tener los inputs y labels “email”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
 
-  it("Despues de que el boton es presionado: tiene que tener el texto “Hemos enviado un correo electrónico”.", () => {});
+    expect(screen.getByText(/email/i));
+  });
 
-  it("Despues de que el boton es presionado: tiene que tener el texto “Si la dirección de correo electrónico que ingresaste está asociada a una cuenta, recibirás un mensaje con instrucciones para restablecer tu contraseña. Por favor, revisa tu bandeja de entrada y sigue las indicaciones.”.", () => {});
+  it("Tiene que tener el boton de “enviar”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
 
-  it("Despues de que el boton es presionado: tiene que tener el texto “Información Adicional: - Verifica también la carpeta de spam o correo no deseado. -Si no recibes el correo en unos minutos, intenta nuevamente o contacta a nuestro soporte.”.", () => {});
+    expect(screen.getByRole("button", { name: "enviar" }));
+  });
+
+  it("Tiene que tener el link de redireccion a iniciar sesion.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
+
+    expect(screen.getByRole("link", { name: /iniciar sesion/i }));
+  });
+
+  it("El link de redir. A iniciar sesion debe redireccionar a “/cuenta/iniciar_sesion”.", async () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
+
+    await userEvent.click(
+      // Click en el link de registrarse
+      screen.getByRole("link", { name: /iniciar sesion/i })
+    );
+
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith("/cuenta/iniciar_sesion");
+  });
+
+  it("Despues de que el boton es presionado: tiene que tener el texto “Hemos enviado un correo electrónico”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
+  });
+
+  it("Despues de que el boton es presionado: tiene que tener el texto “Si la dirección de correo electrónico que ingresaste está asociada a una cuenta, recibirás un mensaje con instrucciones para restablecer tu contraseña. Por favor, revisa tu bandeja de entrada y sigue las indicaciones.”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
+  });
+
+  it("Despues de que el boton es presionado: tiene que tener el texto “Información Adicional: - Verifica también la carpeta de spam o correo no deseado. -Si no recibes el correo en unos minutos, intenta nuevamente o contacta a nuestro soporte.”.", () => {
+    const router = createMockRouter({});
+    render(
+      <AppRouterContext.Provider value={router}>
+        <RecuperarContraseña />
+      </AppRouterContext.Provider>
+    );
+  });
 });
